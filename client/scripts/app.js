ddd23c55cca2f282f59135cb45f50f4c8fc59dea
Nick Havens <npahavens@gmail.com>
2017-06-20 22:33:27 -0500

    added code for app methods

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -4,7 +4,7 @@ let app = {};
 app.init = function() {
 
 };
-
+let url = 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages';
 let clientData = {
   name: 'Nick'
 };
@@ -12,11 +12,48 @@ let clientData = {
 app.send = function(data) {
   //console.log('Data',data);
   $.ajax({
-    url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
+    url: url,
     type: 'POST',
     data: JSON.stringify(data),
     dataType: 'json'
   });
 };
+
+//http://docs.parseplatform.org/rest/guide/#queries
+
+// curl -X GET  -H "X-Parse-Application-Id: 72b8e073a4abde10221ce95f38ed1c63bd7f3d6b"  -H "X-Parse-REST-API-Key: cf1ce23a61e2a40702c347b7dc1e0af8c28f6c7a" http://parse.sfm8.hackreactor.com/chatterbox/classes/messages
+
+app.fetch = function() {
+  $.ajax({
+    type: 'GET',
+    url: url
+  });
+};
+
+app.clearMessages = function() {
+  $('#chats').html('');
+};
+
+app.renderMessage = function(msgObj) {
+  $('#chats').append('<p>' + msgObj.text + '</p>');
+};
+
+app.renderRoom = function(room) {
+  $('#roomSelect').append('<p>' + room + '</p>');
+};
+
+
+
+
+
+
+app.fetch();
+
+
+
+
+
+
+
 
 
