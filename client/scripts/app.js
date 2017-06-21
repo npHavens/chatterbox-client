4ce760a0519ebdc77efd9c078f2c2bcef50a7ccf
Nick Havens <npahavens@gmail.com>
2017-06-20 22:08:30 -0500

    added input form to index.html

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,22 @@
 // YOUR CODE HERE:
+let app = {};
+
+app.init = function() {
+
+};
+
+let clientData = {
+  name: 'Nick'
+};
+
+app.send = function(data) {
+  //console.log('Data',data);
+  $.ajax({
+    url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
+    type: 'POST',
+    data: JSON.stringify(data),
+    dataType: 'json'
+  });
+};
+
+
